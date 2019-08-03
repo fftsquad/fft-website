2618249b332c7608adfd612f34dfef7f005bc085
Greg Jordan <gregljordan@gmail.com>
2019-08-02 22:32:31 -0400

    created navbar skeleton component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <div className="test">
-
+      <Nav />
      </div>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <div className="nav-bar">
+                NavBar goes here
+            </div>
+        </div>
+    )
+}
+export default Nav;
