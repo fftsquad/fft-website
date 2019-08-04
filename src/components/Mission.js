e69871b955051d7edc47fdaca25d637be2dbcfde
Greg Jordan <gregljordan@gmail.com>
2019-08-04 12:49:19 -0400

    Removed nav and footer components except app component

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Nav from "./Nav";
-import Footer from "./Footer";
 import { Breadcrumb } from "react-bootstrap";
 
 const Mission = () => {
     return (
       <div>
-        <Nav />
         <div className="mission">FFT Mission Statement</div>
         breadcrumb on every page except home
         <Breadcrumb>
@@ -16,8 +13,6 @@ const Mission = () => {
           </Breadcrumb.Item>
           <Breadcrumb.Item active>Data</Breadcrumb.Item>
         </Breadcrumb>
-        <Footer />
-        <Footer />
       </div>
     );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,43 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div>
-      <div className="nav-bar">
+    <div style={{ background: "#fff" }}>
+      <div
+        className="navbar navbar-dark bg-dark"
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexDirection: "row",
+          alignItems: " flex-end"
+        }}
+      >
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About Us</Link>
-          <Link to="/mission">Our Mission Statement</Link>
-          <Link to="/blog">FFT Blog</Link>
+          <Link style={{ fontSize: ".1.2rem" }} className="navbar-brand" to="/">
+            Home
+          </Link>
+          <Link
+            style={{ fontSize: ".1.2rem" }}
+            className="navbar-brand"
+            to="/about"
+          >
+            About Us
+          </Link>
+          <Link
+            style={{ fontSize: ".1.2rem" }}
+            className="navbar-brand"
+            to="/mission"
+          >
+            Mission Statement
+          </Link>
+          <Link
+            style={{ fontSize: "1.2rem" }}
+            className="navbar-brand"
+            to="/blog"
+          >
+            FFT Blog
+          </Link>
         </nav>
       </div>
     </div>
